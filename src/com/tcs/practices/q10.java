06fb636889a23b1bc484253effc6ddff6aab78f6
kaviraja05 <kaviyuva2005@gmail.com>
2026-03-15 22:27:32 +0530

    Some codes added

--- src/com/tcs/practices/q10.java
+++ src/com/tcs/practices/q10.java
@@ -1,7 +1,29 @@
-//Move all zeros to the end of an array without using extra arra. Maintain order of nonzero elements
+//Move all zeros to the end of an array without using extra array. Maintain order of nonzero elements
 //i/p: [0,1,0,3,12]
 //o/p: [1,3,12,0,0]
 
 package com.tcs.practices;
+import java.util.*;
 public class q10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+        for(int i = count; i < n; i++){
+            arr[i] = 0;
+        }
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
--- src/com/tcs/practices/q7.java
+++ src/com/tcs/practices/q7.java
@@ -2,5 +2,24 @@
 //output: All prime numbers from 1 to 10 are 2, 3, 5, 7.
 
 package com.tcs.practices;
+import java.util.*;
 public class q7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        for(int i = 2; i <= m; i++){
+            int c = 2;
+            while(c*c <= i){
+                if(i % c == 0){
+                    break;
+                }c++;
+            }
+            if(c*c > i){
+                System.out.println(i);
+            }
+        }
+    }
 }
+
+
+
